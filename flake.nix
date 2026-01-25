8de01bb87c6cb9a8636b997fa823adea96bcd7c1
Anish Pallati <i@anish.land>
2026-01-25 07:20:42 -0500

    style: added and ran formatter

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Configuration";
-  
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -19,58 +19,64 @@
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = inputs@{
-    self,
-    nixpkgs,
-    nix-darwin,
-    home-manager,
-    neovim-nightly-overlay,
-    catppuccin,
-    xdg-termfilepickers,
-  }: {
-    nixosConfigurations.ap-1 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/pc/configuration.nix
-	./hosts/pc/hardware-configuration.nix
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nix-darwin,
+      home-manager,
+      neovim-nightly-overlay,
+      catppuccin,
+      xdg-termfilepickers,
+    }:
+    {
+      formatter = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (
+        system: nixpkgs.legacyPackages.${system}.nixfmt
+      );
 
-	home-manager.nixosModules.home-manager
-	{
-          home-manager.backupFileExtension = "backup";
-	  home-manager.extraSpecialArgs = {
-            inherit inputs xdg-termfilepickers neovim-nightly-overlay;
-	  };
+      nixosConfigurations.ap-1 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/pc/configuration.nix
+          ./hosts/pc/hardware-configuration.nix
 
-	  home-manager.users.anish = {
-            imports = [
-              ./hosts/pc/home.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {
+              inherit inputs xdg-termfilepickers neovim-nightly-overlay;
+            };
 
-	      catppuccin.homeModules.catppuccin
-	      xdg-termfilepickers.homeManagerModules.default
-	    ];
-	  };
-	}
-      ];
+            home-manager.users.anish = {
+              imports = [
+                ./hosts/pc/home.nix
+
+                catppuccin.homeModules.catppuccin
+                xdg-termfilepickers.homeManagerModules.default
+              ];
+            };
+          }
+        ];
+      };
+
+      darwinConfigurations."MacBook-Pro-4" = nix-darwin.lib.darwinSystem {
+        specialArgs = { inherit self; };
+        modules = [
+          ./hosts/macbook/configuration.nix
+
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {
+              inherit inputs neovim-nightly-overlay;
+            };
+
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.anish = import ./hosts/macbook/home.nix;
+          }
+        ];
+      };
     };
-
-    darwinConfigurations."MacBook-Pro-4" = nix-darwin.lib.darwinSystem {
-      specialArgs = { inherit self; };
-      modules = [
-        ./hosts/macbook/configuration.nix
-
-	home-manager.darwinModules.home-manager
-	{
-	  home-manager.backupFileExtension = "backup";
-          home-manager.extraSpecialArgs = {
-	    inherit inputs neovim-nightly-overlay;
-          };
-
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-
-	  home-manager.users.anish = import ./hosts/macbook/home.nix;
-	}
-      ];
-    };
-  };
 }
