e3140158708c1ff5cfa569c2560a44eec8be5b76
Anish Pallati <i@anish.land>
2026-02-01 15:48:49 -0500

    fix: correct urls for homepage-dashboard

--- hosts/pc/configuration/homepage.nix
+++ hosts/pc/configuration/homepage.nix
@@ -14,20 +14,20 @@
         "Media" = [
           {
             "Jellyfin" = {
-              href = "http://localhost:8096";
+              href = "http://pc:8096";
               widget = {
                 type = "jellyfin";
-                url = "http://localhost:8096";
+                url = "http://pc:8096";
                 key = "{{HOMEPAGE_VAR_JELLYFIN_KEY}}";
               };
             };
           }
           {
             "Immich" = {
-              href = "http://localhost:2283";
+              href = "http://pc:2283";
               widget = {
                 type = "immich";
-                url = "http://localhost:2283";
+                url = "http://pc:2283";
                 key = "{{HOMEPAGE_VAR_IMMICH_KEY}}";
               };
             };
@@ -38,19 +38,19 @@
         "Downloads" = [
           {
             "qBittorrent" = {
-              href = "http://localhost:8080";
+              href = "http://pc:8080";
               widget = {
                 type = "qbittorrent";
-                url = "http://localhost:8080";
+                url = "http://pc:8080";
               };
             };
           }
           {
             "Transmission" = {
-              href = "http://localhost:9091";
+              href = "http://pc:9091";
               widget = {
                 type = "transmission";
-                url = "http://localhost:9091";
+                url = "http://pc:9091";
               };
             };
           }
@@ -60,60 +60,60 @@
         "Management" = [
           {
             "Sonarr" = {
-              href = "http://localhost:8989";
+              href = "http://pc:8989";
               widget = {
                 type = "sonarr";
-                url = "http://localhost:8989";
+                url = "http://pc:8989";
                 key = "{{HOMEPAGE_VAR_SONARR_KEY}}";
               };
             };
           }
           {
             "Radarr" = {
-              href = "http://localhost:7878";
+              href = "http://pc:7878";
               widget = {
                 type = "radarr";
-                url = "http://localhost:7878";
+                url = "http://pc:7878";
                 key = "{{HOMEPAGE_VAR_RADARR_KEY}}";
               };
             };
           }
           {
             "Lidarr" = {
-              href = "http://localhost:8686";
+              href = "http://pc:8686";
               widget = {
                 type = "lidarr";
-                url = "http://localhost:8686";
+                url = "http://pc:8686";
                 key = "{{HOMEPAGE_VAR_LIDARR_KEY}}";
               };
             };
           }
           {
             "Readarr" = {
-              href = "http://localhost:8787";
+              href = "http://pc:8787";
               widget = {
                 type = "readarr";
-                url = "http://localhost:8787";
+                url = "http://pc:8787";
                 key = "{{HOMEPAGE_VAR_READARR_KEY}}";
               };
             };
           }
           {
             "Prowlarr" = {
-              href = "http://localhost:9696";
+              href = "http://pc:9696";
               widget = {
                 type = "prowlarr";
-                url = "http://localhost:9696";
+                url = "http://pc:9696";
                 key = "{{HOMEPAGE_VAR_PROWLARR_KEY}}";
               };
             };
           }
           {
             "Bazarr" = {
-              href = "http://localhost:6767";
+              href = "http://pc:6767";
               widget = {
                 type = "bazarr";
-                url = "http://localhost:6767";
+                url = "http://pc:6767";
                 key = "{{HOMEPAGE_VAR_BAZARR_KEY}}";
               };
             };
@@ -124,14 +124,14 @@
         "Tools" = [
           {
             "Vaultwarden" = {
-              href = "http://localhost:8222";
+              href = "http://pc:8222";
               icon = "vaultwarden";
               description = "Password manager";
             };
           }
           {
             "Sunshine" = {
-              href = "https://localhost:47990";
+              href = "https://pc:47990";
               icon = "sunshine";
               description = "Game streaming";
             };
