74cb052962d3f6d5383423189b33c94cf141e376
Anish Pallati <i@anish.land>
2026-02-01 13:41:31 -0500

    fix: use lib.mkMerge instead of //

--- modules/system/localsend.nix
+++ modules/system/localsend.nix
@@ -5,12 +5,15 @@
   ...
 }:
 
-{
-  environment.systemPackages = with pkgs; [ localsend ];
-}
-// lib.optionalAttrs isLinux {
-  programs.localsend = {
-    enable = true;
-    openFirewall = true;
-  };
-}
+lib.mkMerge [
+  {
+    environment.systemPackages = with pkgs; [ localsend ];
+  }
+
+  (lib.optionalAttrs isLinux {
+    programs.localsend = {
+      enable = true;
+      openFirewall = true;
+    };
+  })
+]
--- modules/system/tailscale.nix
+++ modules/system/tailscale.nix
@@ -6,28 +6,31 @@
   ...
 }:
 
-{
-  services.tailscale.enable = true;
-}
-// lib.optionalAttrs isLinux {
-  services.tailscale.useRoutingFeatures = "both"; # IP forwarding
+lib.mkMerge [
+  {
+    services.tailscale.enable = true;
+  }
 
-  # Enable UDP GRO forwarding on boot
-  systemd.services.tailscale-udp-gro = {
-    description = "Enable UDP GRO forwarding for Tailscale";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
+  (lib.optionalAttrs isLinux {
+    services.tailscale.useRoutingFeatures = "both"; # IP forwarding
+
+    # Enable UDP GRO forwarding on boot
+    systemd.services.tailscale-udp-gro = {
+      description = "Enable UDP GRO forwarding for Tailscale";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      script = ''
+        # Get the default route interface
+        NETDEV=$(${pkgs.iproute2}/bin/ip -o route get 8.8.8.8 | cut -f 5 -d " ")
+        if [ -n "$NETDEV" ]; then
+          ${pkgs.ethtool}/bin/ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off || true
+        fi
+      '';
     };
-    script = ''
-      # Get the default route interface
-      NETDEV=$(${pkgs.iproute2}/bin/ip -o route get 8.8.8.8 | cut -f 5 -d " ")
-      if [ -n "$NETDEV" ]; then
-        ${pkgs.ethtool}/bin/ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off || true
-      fi
-    '';
-  };
-}
+  })
+]
