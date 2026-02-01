a98ee9099c9d79a1f800203251544fde351d66ee
Anish Pallati <i@anish.land>
2026-02-01 17:19:29 -0500

    fix: set setgid bit so new files inherit media group

--- hosts/pc/configuration/media.nix
+++ hosts/pc/configuration/media.nix
@@ -120,12 +120,12 @@
 
   # Directories
   systemd.tmpfiles.rules = [
-    "d /media 0775 root media -"
-    "d /media/downloads 0775 root media -"
-    "d /media/downloads/.incomplete 0775 root media -"
-    "d /media/movies 0775 root media -"
-    "d /media/tv 0775 root media -"
-    "d /media/music 0775 root media -"
-    "d /media/books 0775 root media -"
+    "d /media 2775 root media -"
+    "d /media/downloads 2775 root media -"
+    "d /media/downloads/.incomplete 2775 root media -"
+    "d /media/movies 2775 root media -"
+    "d /media/tv 2775 root media -"
+    "d /media/music 2775 root media -"
+    "d /media/books 2775 root media -"
   ];
 }
