d3e615dc8fd84b2408a92b0dcb6b2cff203ac02a
Anish Pallati <i@anish.land>
2026-02-05 00:07:11 -0500

    feat: update firefox settings

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -10,8 +10,15 @@
     package = pkgs.firefox-bin;
 
     policies = {
+      DisableAppUpdate = true;
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
+      DNSOverHTTPS = {
+        Enabled = true;
+        ProviderURL = "https://mozilla.cloudflare-dns.com/dns-query";
+        Fallback = true;
+        Locked = true;
+      };
     };
 
     profiles.default = {
@@ -36,7 +43,6 @@
 
           # Misc
           bitwarden
-          simple-tab-groups
         ];
       };
 
@@ -54,43 +60,99 @@
         };
       };
 
+      # privacy settings mostly adapted from https://brainfucksec.github.io/firefox-hardening-guide-2026
       settings = {
         # general
         "browser.startup.page" = 3; # Open previous windows and tabs
         "sidebar.verticalTabs" = true;
 
-        # disable suggestions
-        "browser.search.suggest.enabled" = false;
-        "browser.urlbar.suggest.searches" = false;
+        # startup settings
+        "browser.aboutConfig.showWarning" = false; # disable about:config warning
+        "browser.newtabpage.activity-stream.showSponsored" = false; # disable sponsored content on Firefox Home
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
+        "browser.newtabpage.activity-stream.default.sites" = ""; # clear default topsites
+
+        # recommendations
+        "extensions.getAddons.showPane" = false;
+        "extensions.htmlaboutaddons.recommendations.enabled" = false;
+        "browser.discovery.enabled" = false;
+
+        # telemetry
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false; # disable Firefox Home
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false; # disable new data submission
+        "datareporting.healthreport.uploadEnabled" = false; # disable Health Reports
+        "toolkit.telemetry.enabled" = false; # disable general telemetry
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.server" = "data:,";
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false; # (Background Hang Reporter)
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.coverage.opt-out" = true;
+        "identity.fxaccounts.telemetry.clientAssociationPing.enabled" = false;
+        "toolkit.coverage.opt-out" = true;
+        "toolkit.coverage.endpoint.base" = "";
+
+        # studies
+        "app.shield.optoutstudies.enabled" = false;
+        "app.normandy.enabled" = false;
+        "app.normandy.api_url" = "";
+
+        # crash reports
+        "breakpad.reportURL" = "";
+        "browser.tabs.crashReporting.sendReport" = false;
+
+        # search bar
         "browser.urlbar.showSearchSuggestionsFirst" = false;
-        "browser.search.suggest.enabled.private" = false;
-        "browser.urlbar.trending.featureGate" = false;
-        "browser.urlbar.recentsearches.featureGate" = false;
-        "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+        "browser.urlbar.quicksuggest.enabled" = false; # disable location bar contextual suggestions
         "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
-        "browser.urlbar.quicksuggest.enabled" = false;
-        "browser.urlbar.suggest.topsites" = false;
-        "browser.urlbar.suggest.history" = false;
+        "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+        "browser.search.suggest.enabled" = false; # disable live search suggestions
+        "browser.search.suggest.enabled.private" = false;
         "browser.urlbar.suggest.bookmark" = false;
-        "browser.urlbar.suggest.openpage" = false;
         "browser.urlbar.suggest.engines" = false;
+        "browser.urlbar.suggest.history" = false;
+        "browser.urlbar.suggest.openpage" = false;
         "browser.urlbar.suggest.quickactions" = false;
+        "browser.urlbar.suggest.searches" = false;
+        "browser.urlbar.suggest.topsites" = false;
+        "browser.urlbar.trending.featureGate" = false; # disable urlbar trending search suggestions
+        "browser.urlbar.addons.featureGate" = false; # disable urlbar suggestions
+        "browser.urlbar.amp.featureGate" = false;
+        "browser.urlbar.mdn.featureGate" = false;
+        "browser.urlbar.recentsearches.featureGate" = false;
+        "browser.urlbar.weather.featureGate" = false;
+        "browser.urlbar.wikipedia.featureGate" = false;
+        "browser.urlbar.yelp.featureGate" = false;
+
+        # passwords / forms
+        "signon.rememberSignons" = false; # disable saving passwords
+        "signon.autofillForms" = false; # disable autofill login and passwords
+        "signon.formlessCapture.enabled" = false; # disable formless login capture for Password Manager
+        "browser.formfill.enable" = false; # disable search and form history
+        "extensions.formautofill.addresses.enabled" = false; # disable form autofill
+        "extensions.formautofill.creditCards.enabled" = false;
+
+        # downloads
+        "browser.download.manager.addToRecentDocs" = false; # do not use system's "recent downloads" list
+        "browser.download.always_ask_before_handling_new_types" = true; # always ask before handling new mimetypes
 
         # privacy
+        "network.http.referer.XOriginTrimmingPolicy" = 2; # trim referrer to origin
+        "browser.contentblocking.category" = "strict"; # Total Cookie Protection
         "privacy.globalprivacycontrol.enabled" = true;
-        "signon.rememberSignons" = false;
-        "extensions.formautofill.creditCards.enabled" = false;
-        "extensions.formautofill.addresses.enabled" = false;
-        "datareporting.healthreport.uploadEnabled" = false;
-        "datareporting.policy.dataSubmissionEnabled" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.archive.enabled" = false;
-        "identity.fxaccounts.telemetry.clientAssociationPing.enabled" = false;
+        "privacy.resistFingerprinting" = true;
+        "privacy.resistFingerprinting.block_mozAddonManager" = true; # disable mozAddonManager Web API
 
-        # dns over https
-        "network.trr.mode" = 2; # fall back to local (tailscale) on failure
-        "network.trr.uri" = "https://mozilla.cloudflare-dns.com/dns-query";
+        # enable webauth
+        "security.webauth.u2f" = true;
+        "security.webauth.webauthn" = true;
+        "security.webauth.webauthn_enable_softtoken" = true;
+        "security.webauth.webauthn_enable_usbtoken" = true;
 
         # gwfox theme requirements
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
