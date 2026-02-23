83a5a9689db5235ed25a43a2486e14769cc22ef9
Anish Pallati <i@anish.land>
2026-02-23 14:23:56 -0500

    feat: temporarily remove moonlight

--- modules/common/discord.nix
+++ modules/common/discord.nix
@@ -1,20 +1,10 @@
-{ inputs, ... }:
+{ ... }:
 {
   flake.modules.homeManager.discord =
     { pkgs, ... }:
     {
-      imports = [ inputs.moonlight.homeModules.default ];
+      home.packages = [ pkgs.discord-canary ];
 
-      home.packages = [
-        (pkgs.discord-canary.override {
-          withMoonlight = true;
-          moonlight = inputs.moonlight.packages.${pkgs.stdenv.hostPlatform.system}.moonlight;
-        })
-      ];
-
-      programs.moonlight = {
-        enable = true;
-        configs.canary = { };
-      };
+      # TODO: add moonlight
     };
 }
