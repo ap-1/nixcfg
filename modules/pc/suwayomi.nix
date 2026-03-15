6eb84b208ceb77bf936246177428159c1b58e1ac
Anish Pallati <i@anish.land>
2026-03-15 04:04:02 -0400

    feat: set suwayomi settings

--- modules/pc/suwayomi.nix
+++ modules/pc/suwayomi.nix
@@ -8,8 +8,14 @@
         settings.server = {
           ip = "0.0.0.0";
           port = 4567;
+          autoDownloadNewChapters = true;
+          excludeEntryWithUnreadChapters = true;
           flareSolverrEnabled = true;
           flareSolverrUrl = "http://localhost:8191";
+          extensionRepos = [
+            "https://raw.githubusercontent.com/suwayomi/tachiyomi-extension/repo/index.min.json"
+            "https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json"
+          ];
         };
       };
     };
