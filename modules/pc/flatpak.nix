55f5aa3aa6fe3677447521f028ad430977689859
Anish Pallati <i@anish.land>
2026-03-15 03:30:28 -0400

    feat: remove mangohud from flatpak packages

--- modules/pc/flatpak.nix
+++ modules/pc/flatpak.nix
@@ -13,7 +13,6 @@
 
         packages = [
           "org.vinegarhq.Sober"
-          "org.freedesktop.Platform.VulkanLayer.MangoHud"
         ];
 
         overrides = {
@@ -22,14 +21,9 @@
               filesystems = [
                 "xdg-run/app/com.discordapp.Discord:create"
                 "xdg-run/discord-ipc-0"
-                "xdg-config/MangoHud:ro"
               ];
               talk-name = [ "com.feralinteractive.GameMode" ]; # talk to GameMode daemon
               devices = [ "input" ];
-            };
-            Environment = {
-              MANGOHUD = "1";
-              MANGOHUD_CONFIGFILE = "/home/anish/.config/MangoHud/MangoHud.conf";
             };
           };
         };
