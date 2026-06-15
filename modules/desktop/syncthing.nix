d53b42179bb11c6a38ec1d4a6f26e2ec86b04196
Anish Pallati <i@anish.land>
2026-06-15 04:48:52 -0400

    fix: avoid overriding host for syncthing

--- modules/desktop/syncthing.nix
+++ modules/desktop/syncthing.nix
@@ -4,16 +4,13 @@ let
 in
 {
   flake.modules.homeManager.syncthing =
-    { pkgs, config, ... }:
+    { pkgs, ... }:
     {
       home.packages = [ pkgs.syncthing ];
 
       services.syncthing = {
         enable = true;
-        extraOptions = [
-          "--gui-address=127.0.0.1:8384"
-          "--home=${config.xdg.stateHome}/syncthing"
-        ];
+        extraOptions = [ "--gui-address=127.0.0.1:8384" ];
 
         settings = {
           gui.insecureSkipHostcheck = true;
