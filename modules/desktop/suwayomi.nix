664931b3e92fbcb7c6ec847f54ac86d458ceb111
Anish Pallati <i@anish.land>
2026-06-17 01:06:13 -0400

    fix: enable flaresolverr response fallback for suwayomi

--- modules/desktop/suwayomi.nix
+++ modules/desktop/suwayomi.nix
@@ -18,6 +18,7 @@
           excludeEntryWithUnreadChapters = true;
           flareSolverrEnabled = true;
           flareSolverrUrl = "http://localhost:8191";
+          flareSolverrAsResponseFallback = true;
           extensionRepos = [
             "https://raw.githubusercontent.com/suwayomi/tachiyomi-extension/repo/index.min.json"
             "https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json"
