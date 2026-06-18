19b4c4ba1d5b70c23541614aae8e78fdd48064ab
Anish Pallati <i@anish.land>
2026-06-18 00:39:35 -0400

    feat: add jujutsu to shared shell

--- modules/common/git.nix
+++ modules/common/git.nix
@@ -27,6 +27,23 @@ in
         };
       };
 
+      programs.jujutsu = {
+        enable = true;
+        settings = {
+          user = {
+            name = meta.name;
+            email = meta.email;
+          };
+          signing = {
+            backend = "gpg";
+            key = "B1CA832135A8C503";
+            behavior = "own";
+          };
+          ui.default-command = "log";
+          templates.commit_trailers = ''format_signed_off_by_trailer(self)'';
+        };
+      };
+
       programs.gh = {
         enable = true;
         gitCredentialHelper.enable = true;
