519e0d2aa92bb9049abceef91fde0f7ff479ea7e
Anish Pallati <i@anish.land>
2026-06-18 00:43:26 -0400

    fix: enable focus_on_activate for portal file chooser

--- modules/desktop/hyprland/hyprland.lua
+++ modules/desktop/hyprland/hyprland.lua
@@ -202,8 +202,9 @@ hl.config({
 
 hl.config({
     misc = {
-        force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo   = true,  -- If true disables the random hyprland logo / anime girl background. :(
+        force_default_wallpaper  = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo    = true,  -- If true disables the random hyprland logo / anime girl background. :(
+        focus_on_activate        = true,  -- Focus windows that request activation (e.g. portal file chooser)
     },
 })
 
